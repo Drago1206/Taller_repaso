531c50530e01423989aeaa1f27a4f64d6f690ffe
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-11-01 10:54:50 -0500

    Actividad_2

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 addEventListener("DOMContentLoaded", (e)=>{
-    const form= document.querySelector(".form")
+    const form= document.querySelector("#form")
     form.addEventListener("submit",(e)=>{
-    e.preventDefault
+    e.preventDefault()
     let datainput=Object.fromEntries(new FormData(e.target));
-
-    let horas = document.querySelector(".horas")
-    horas.contains(".horas")
-    let trab= querySelector(".trab")
-    if (trab==true){
-        for(let i=0; i<trab; i++){
-        let nomina= (5080*horas)
+    let horas = datainput.horas
+    let nombre = datainput.nombre
+ //   let horas = document.querySelector("#horas")
+    //horas.contains(".horas")
+    //let trab= querySelector("#nombre")
+    let nomina= (5,208.33*horas)
+    if (nombre!=''){
             let tabla= document.querySelector("tbody")
-            tabla.innerHTML="";
-            tabla.appendChild("beforeend",`
+            tabla.insertAdjacentHTML("beforeend",`
             <tr>
-            <td>${data[trab]} </td>
-            <td>${data[horas]} </td>
-            <td>${data[nomina]} </td>
+            <td>${nombre} </td>
+            <td>${horas} </td>
+            <td>${nomina} </td>
             </tr>
             `)
 
             
-        }
-    }      
+        }    
     })
 
 })
