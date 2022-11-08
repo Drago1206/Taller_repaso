0991cb73962df03a2654b40c136d305af4b43f5e
JhoanSebastianGelvesHernandezADSO <jhoangelves1205@gmail.com>
2022-11-08 09:54:51 -0500

    3

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-addEventListener("DOMContentLoaded", (e)=>{
-    const form= document.querySelector("#form")
-    form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-    let datainput=Object.fromEntries(new FormData(e.target));
-    let horas = datainput.horas
-    let nombre = datainput.nombre
- //   let horas = document.querySelector("#horas")
-    //horas.contains(".horas")
-    //let trab= querySelector("#nombre")
-    let nomina= (5,208.33*horas)
-    if (nombre!=''){
-            let tabla= document.querySelector("tbody")
-            tabla.insertAdjacentHTML("beforeend",`
-            <tr>
-            <td>${nombre} </td>
-            <td>${horas} </td>
-            <td>${nomina} </td>
-            </tr>
-            `)
+addEventListener("DOMContentLoaded", (e) => {
+    let N = document.querySelector("#N")
+    let resultado = ""
+    let form = document.querySelector("#form")
+    let suma = 0
+    let sobr = document.querySelector("#resultados")
 
-            
-        }    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let datosp = Object.fromEntries(new FormData(e.target));
+        for (let i = 1; i < datosp.N; s++) {
+            if (datosp.N % i == 0) {
+                suma += i
+
+            }
+        }
+        if (datosp.N == suma) {
+            resultado = "Es perfecto"
+            suma = 0
+        }
+        else {
+            resultado = "No es perfecto"
+            suma = 0
+        }
+
+        sobr.innerHTML = ""
+        sobr.insertAdjacentHTML("beforeend", `<h2> ${datosp.N + ' ' + resultado} </h2>`);
+
     })
 
 })
